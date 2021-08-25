fc73eb2071f43ca19537718d5dd1313a54bf7eba
HammadUllah25 <49758382+HammadUllah25@users.noreply.github.com>
2021-08-25 18:33:33 +0500

    Project

---
+++ PhotoEditor/app/src/main/java/com/btc/photoeditorapp/Application.java
@@ -0,0 +1,15 @@
+package com.btc.photoeditorapp;
+
+import ly.img.android.ImgLySdk;
+
+
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ImgLySdk.init(this);
+    }
+
+
+}
